41f93a05b0e097e4420e702e849aa5b81cee9a6a
ameencaslam <ameencaslam@gmail.com>
2024-08-17 15:11:25 +0530

    fixed the password requirement color problem

--- mode.js
+++ mode.js
@@ -6,10 +6,14 @@
     if (modeValue === 0) {
       toggleButton.style.backgroundImage = "url(moon.png)";
       document.documentElement.style.filter = "invert(100%)";
+      document.getElementById("passwordRequirements").style.filter =
+        "invert(100%)";
       modeValue = 1;
     } else {
       toggleButton.style.backgroundImage = "url(sun.png)";
       document.documentElement.style.filter = "invert(0%)";
+      document.getElementById("passwordRequirements").style.filter =
+        "invert(0%)";
       modeValue = 0;
     }
   });
